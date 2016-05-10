3607fb63f78811449e4021d37464db3ed5171473
Melvin Iwuajoku <melvin.iwuajoku@stockport.gov.uk>
2016-05-10 16:10:26 +0100

    chnaged files

---
+++ Contexts/DomainModelPostgreSqlContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aspnet5api.Models;
+using Microsoft.Data.Entity;
+
+namespace aspnet5api.Contexts
+{
+    public class DomainModelPostgreSqlContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        // override Onm
+
+    }
+}
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Setting ENV variables for cntlm..."
-export HTTP_PROXY=http://10.0.2.2:3128
-export HTTPS_PROXY=http://10.0.2.2:3128
-export NO_PROXY=192.168.99.*,*.local.dev
+#export HTTP_PROXY=http://10.0.2.2:3128
+#export HTTPS_PROXY=http://10.0.2.2:3128
+#export NO_PROXY=192.168.99.*,*.local.dev
 
 docker build --build-arg http_proxy=http://10.0.2.2:3128 -t api .
 docker run -p 7000:7000 api
